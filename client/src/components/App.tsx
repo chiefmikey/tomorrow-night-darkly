08cfde867f43112dbff939e079f0fd3e83664520
chiefmikey <wolfemikl@gmail.com>
2022-02-21 22:10:47 -0700

    Update page

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
+import { FiChrome } from 'react-icons/fi';
+import { RiFirefoxLine } from 'react-icons/ri';
 
 const App = () => {
   return (
     <div className="app">
       <div className="title">
-        <h1>Tomorrow</h1>
-        <h2>Night</h2>
-        <h3>Darkly</h3>
+        <div className="title-1">Tomorrow</div>
+        <div className="title-2">Night</div>
+        <div className="title-3">Darkly</div>
       </div>
       <div className="content">
-        <h4>Install Theme</h4>
-        <div className="chrome">
-          <div className="chrome-icon"></div>
-          <div className="chrome-text">Chrome</div>
-        </div>
-        <div className="firefox">
-          <div className="firefox-icon"></div>
-          <div className="firefox-text">Firefox</div>
+        <div className="content-title">Install Theme</div>
+        <div className="content-selection">
+          <a
+            href="https://chrome.google.com/webstore/detail/tomorrow-night-darkly/najhldfogkjhgdaaloddlfdgjfolnoik"
+            className="content-option"
+          >
+            <div className="chrome-icon">
+              <FiChrome />
+            </div>
+            <div className="content-text">Chrome</div>
+          </a>
+          <a
+            href="https://addons.mozilla.org/en-US/firefox/addon/tomorrow-night-darkly"
+            className="content-option"
+          >
+            <div className="firefox-icon">
+              <RiFirefoxLine />
+            </div>
+            <div className="content-text">Firefox</div>
+          </a>
         </div>
       </div>
     </div>
