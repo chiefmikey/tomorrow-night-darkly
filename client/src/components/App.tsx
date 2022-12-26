61686fab66ee40a123a631f713c872812c785d30
Mikl Wolfe <wolfemikl@gmail.com>
2022-12-25 23:20:32 -0700

    Update formatting

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -2,52 +2,50 @@ import React from 'react';
 import Img from 'react-cool-img';
 import { FiChrome } from 'react-icons/fi';
 
-const App = () => {
-  return (
-    <div className="background">
-      <div className="app">
-        <div className="title">
-          <span className="title-1">Tomorrow</span>
-          <span className="title-2">Night</span>
-          <span className="title-3">Darkly</span>
-        </div>
-        <Img
-          className="logo"
-          alt="Tomorrow Night Darkly logo"
-          src="public/assets/img/tomorrow-night-darkly.svg"
-          lazy={false}
-          decode={false}
-        />
-        <div className="content">
-          <span className="content-title">
-            A <span className="simply-dark">Simply Dark</span> Theme
-          </span>
-          <div className="content-selection">
-            <a
-              href="https://chrome.google.com/webstore/detail/tomorrow-night-darkly/najhldfogkjhgdaaloddlfdgjfolnoik"
-              className="content-option"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="chrome-icon">
-                <FiChrome />
-              </div>
-              <span className="content-text">Chrome</span>
-            </a>
-          </div>
-        </div>
-        <span className="made-by">
+const App = () => (
+  <div className="background">
+    <div className="app">
+      <div className="title">
+        <span className="title-1">Tomorrow</span>
+        <span className="title-2">Night</span>
+        <span className="title-3">Darkly</span>
+      </div>
+      <Img
+        className="logo"
+        alt="Tomorrow Night Darkly logo"
+        src="public/assets/img/tomorrow-night-darkly.svg"
+        lazy={false}
+        decode={false}
+      />
+      <div className="content">
+        <span className="content-title">
+          A <span className="simply-dark">Simply Dark</span> Theme
+        </span>
+        <div className="content-selection">
           <a
-            href="https://github.com/chiefmikey"
+            href="https://chrome.google.com/webstore/detail/tomorrow-night-darkly/najhldfogkjhgdaaloddlfdgjfolnoik"
+            className="content-option"
             target="_blank"
             rel="noreferrer"
           >
-            Made by Chief Mikey
+            <div className="chrome-icon">
+              <FiChrome />
+            </div>
+            <span className="content-text">Chrome</span>
           </a>
-        </span>
+        </div>
       </div>
+      <span className="made-by">
+        <a
+          href="https://github.com/chiefmikey"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Made by Chief Mikey
+        </a>
+      </span>
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
